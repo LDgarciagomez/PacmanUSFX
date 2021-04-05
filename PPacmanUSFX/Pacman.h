cfeb26346aa3c2e849efdb1a5a5caae17fea8ba3
lautaro garcia <lautarogarcia_234@hotmail.com>
2021-04-05 12:19:23 -0400

    Texturas update

--- PPacmanUSFX/Pacman.h
+++ PPacmanUSFX/Pacman.h
@@ -5,19 +5,26 @@ class Pacman
 private:
 	SDL_Point posicion;
 	int color;
-	float velocidadX;
-	float velocidadY;
+
+
+	SDL_Point velocidad;
+	int color;
+
 public:
 	void move(float _velocidadX, float velocidadY);
 
 	SDL_Point getPosicion() { return posicion;  }
-	void  setPosicionX(SDL_Point _posicion) { posicion = _posicion; }
+	void  setPosicion(SDL_Point _posicion) { posicion = _posicion; }
 	
+	//** OTRA FORMA**
+	//int getPosicionX() { return posicion.x; }
+	//int getPosicionY() { return posicion.y; }
+	//void setPosicionX(int _x) { posicion.x = _x; }
+	//void setPosicionX(int _y) { posicion.y = _y;  }
 
-	float getVelocidadX() { return velocidadX ; }
-	void  setVelocidadX(float _velocidadX) { velocidadX = _velocidadX; }
-	float getVelocidadY() { return velocidadY; }
-	void  setVelocidadY(float _velocidadY) { velocidadY = _velocidadY; }
+	SDL_Point getVelocidad() { return velocidad; }
+	void  setVelocidad(SDL_Point _velocidad) { velocidad = _velocidad; }
+	
 
 
 
