8646779c6c260a15590d04d9715076961d5f3577
lautaro garcia <lautarogarcia_234@hotmail.com>
2021-04-08 13:13:10 -0400

    Pacman v01

--- PPacmanUSFX/Game_manager.h
+++ PPacmanUSFX/Game_manager.h
@@ -1,5 +1,55 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+
+#include <SDL.h>
+
+#include "Pacman.h"
+#include "Fantasma.h"
+#include "Frutas.h"
+
+using namespace std;
+
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
 class Game_manager
 {
-};
+private:
+    bool juego_en_ejecucion;
 
+    //The window we'll be rendering to
+    SDL_Window* gWindow;
+
+    //The window renderer
+    SDL_Renderer* gRenderer;
+
+    //The surface contained by the window
+    SDL_Surface* gScreenSurface;
+
+    //The images we will load and show on the screen
+    SDL_Surface* gPacManSurface;
+    SDL_Surface* gFantasmaSurface;
+    SDL_Surface* gFrutaSurface;
+
+public:
+    Game_manager();
+    int onExecute();
+
+    bool onInit();
+
+    void onEvent(SDL_Event* Event);
+    void onLoop();
+    void onRender();
+    void onCleanup();
+
+    /*  SDL_Surface* loadMediaToSurface(string _mediaFile);*/
+
+    Pacman* pacman;
+    Fantasma fantasma;
+    Fruta* fruta;
+};
--- PPacmanUSFX/Pacman.h
+++ PPacmanUSFX/Pacman.h
@@ -1,37 +1,46 @@
 #pragma once
+#include <iostream>
+#include <string>
 #include <SDL.h>
+//#include "Movimientos.h"
+//#include "Textura.h"
+using namespace std;
+
 class Pacman
 {
 private:
 	SDL_Point posicion;
-	int color;
-
-
 	SDL_Point velocidad;
-	int color;
+
+	int ancho;
+	int alto;
 
 public:
-	void move(float _velocidadX, float velocidadY);
+	const int SCREEN_WIDTH = 640;
+	const int SCREEN_HEIGHT = 480;
 
-	SDL_Point getPosicion() { return posicion;  }
-	void  setPosicion(SDL_Point _posicion) { posicion = _posicion; }
-	
-	//** OTRA FORMA**
-	//int getPosicionX() { return posicion.x; }
-	//int getPosicionY() { return posicion.y; }
-	//void setPosicionX(int _x) { posicion.x = _x; }
-	//void setPosicionX(int _y) { posicion.y = _y;  }
-	//xd
-
-	SDL_Point getVelocidad() { return velocidad; }
-	void  setVelocidad(SDL_Point _velocidad) { velocidad = _velocidad; }
-	
+	const int PACMAN_WIDTH = 20;
+	const int PACMAN_HEIGHT = 20;
+	const int PACMAN_VEL = 10;
 
 
+	Pacman();
+	void handleEvent(SDL_Event& e);
+	void move();
+	void render();
+
+	//The window we'll be rendering to
+	SDL_Window* window = NULL;
+
+	//The window renderer
+	SDL_Renderer* renderer = NULL;
+
+	//The surface contained by the window
+	SDL_Surface* screenSurface = NULL;
 
 
-
-
+	//Scene textures
+	//Textura* PacmanTextura;
+	SDL_Surface* pacmanSurface = NULL;
 
 };
-
